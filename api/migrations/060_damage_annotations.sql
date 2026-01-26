7b634e1e4d903ef2949b958bf982838900a12b66
Jay <jay@officeintegrator.com>
2026-01-25 20:20:23 -0500

    Revert "Revert "Remove nonessential files and directories""
    This reverts commit e60e201bbea1395ce1c296e6e4968635520a9af5.

--- api/migrations/060_damage_annotations.sql
+++
@@ -1,9 +0,0 @@
--- Adds world-space damage annotations and UV polygon overlay support
-ALTER TABLE IF EXISTS damage_reports
-  ADD COLUMN IF NOT EXISTS damage_annotations JSONB NOT NULL DEFAULT '[]'::jsonb;
-
-ALTER TABLE IF EXISTS damage_reports
-  ADD COLUMN IF NOT EXISTS damage_overlay JSONB NOT NULL DEFAULT '{}'::jsonb;
-
-CREATE INDEX IF NOT EXISTS idx_damage_reports_annotations_gin
-  ON damage_reports USING GIN (damage_annotations);
