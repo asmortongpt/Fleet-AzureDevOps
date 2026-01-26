f01803e76170008f8ade0acbbdf80031dcd1cbfd
Jay <jay@officeintegrator.com>
2026-01-25 20:20:23 -0500

    Revert "Revert "Remove nonessential files and directories""
    This reverts commit c2712091b305c9f30c8bb60cb7f8d8092508478d.

--- services/threats/app/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Threat Detection Service for Radio Fleet Dispatch
-
-Agent 017: Comprehensive threat detection, behavioral analysis,
-and automated threat hunting system.
-"""
-
-__version__ = "1.0.0"
--- tools/demo-suite/emulators/README.md
+++
@@ -1,1 +0,0 @@
-Emulators are managed by services/emulator-manager in v2.
